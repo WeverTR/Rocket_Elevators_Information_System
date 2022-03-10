084792de8a4764f28409517b2fadeb2dcf23ced4
troywever@gmail.com <troywever@gmail.com>
2022-03-10 13:59:11 -0500

    Web app working, ready for deployment, not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
   # get 'home/index'
-  root 'home#index'
-=======
-=======
-  resources :quotes
-  # get 'home/index'
-  root 'home#index'
   # resources :employees
   # resources : user; only : [:new, :create, :edit, :update, :show, :destroy]
 
@@ -18,17 +8,13 @@ Rails.application.routes.draw do
   # post '/login', to: 'session#crate'
   # post '/login', to:'session#destroy'
   # get '/logout', to: 'session#destroy'
->>>>>>> 5ec3c32e133ecc89c65e8910835275182c0a14f1
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
   get "/index", to:"pages#index"
   get "/quote", to:"pages#quote"
   get "/corporate", to:"pages#corporate"
   get "/residential", to:"pages#residential"
-<<<<<<< HEAD
->>>>>>> 8d9194f26fdb195551d73cf5c46bd55cb8491805
-=======
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
->>>>>>> 5ec3c32e133ecc89c65e8910835275182c0a14f1
+  resources :quotes
 end
