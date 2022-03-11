e98a10dfe64bcd97b40bba27c93e46be875ba896
troywever@gmail.com <troywever@gmail.com>
2022-03-11 02:10:31 -0500

    one more time deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,5 +61,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.hosts << "TroyWever"
+  # config.hosts << "TroyWever"
 end
