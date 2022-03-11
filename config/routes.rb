568e7b0ac353b2b38fcf4ab482bafce36b208fdc
troywever@gmail.com <troywever@gmail.com>
2022-03-10 23:21:49 -0500

    Devise + Rails_Admin working, employee can sign in and access quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
 Rails.application.routes.draw do
-  # get 'home/index'
-  resources :quotes
-  root 'home#index'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  # resources :employees
-  # resources : user; only : [:new, :create, :edit, :update, :show, :destroy]
-
-  # #session routes
-  # get '/login', to: 'session#login'
-  # post '/login', to: 'session#crate'
-  # post '/login', to:'session#destroy'
-  # get '/logout', to: 'session#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
   get "/index", to:"pages#index"
   get "/quote", to:"pages#quote"
   get "/corporate", to:"pages#corporate"
   get "/residential", to:"pages#residential"
   resources :quotes
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
 end
