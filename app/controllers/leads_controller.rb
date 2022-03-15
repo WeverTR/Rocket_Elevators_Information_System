e0aafb4aeb9671f0ef73caa10ff4f78e52a90d2e
WaelHermassi <h_wael2007@hotmail.com>
2022-03-15 16:06:02 -0400

    up

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,11 +1,12 @@
 class LeadsController < ApplicationController
     skip_before_action :verify_authenticity_token
       
+    
     def new
-        @leads = Leads.new
+        @leads = Leads.new(leads_params)
     end
     def create
-
+       
         @FullName = params[:FullName]
         @CompanyName = params[:CompanyName]
         @Email = params[:Email]
@@ -16,10 +17,13 @@ class LeadsController < ApplicationController
         @Message = params[:Message]
 
         @leads = Leads.new(FullName: @FullName, CompanyName: @CompanyName, Email: @Email, Phone: @Phone, ProjectName: @ProjectName, ProjectDescription: @ProjectDescription, DepartmentInCharge: @DepartmentInCharge, Message: @Message)
-        
+        @leads.attachment.attach(params[:attachment])
             if @leads.save
                 redirect_to index_path
             end
     end
+    def leads_params
+        params.require(:leads).permit(:FullName, :CompanyName, :Email, :Phone, :ProjectName, :ProjectDescription, :DepartmentInCharge, :Message, attachment:[])
+      end
 end
 
