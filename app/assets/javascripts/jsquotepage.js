858b6c5966a672b1f4abc0e86e87baa3b98a228d
rolens <rolenstek@gmail.com>
2022-03-17 15:35:16 -0400

    working on the script

--- app/assets/javascripts/jsquotepage.js
+++ app/assets/javascripts/jsquotepage.js
@@ -1,199 +1,79 @@
-window.onload = () => elevatorUnitPrice()
- 
-let buildingType;
+var documentready = function(){
+    start()
+}
+$(document). on('turbolinks:load',  documentready); 
 
-$("#building-type").change(pick)
+const buildingTypes = {
+	commercial: ["number-of-floors", "number-of-basements","number-of-companies", "number-of-parking-spots", "number-of-elevators", "elevator-amount"],
+	residential: ["number-of-apartments", "number-of-floors", "number-of-basements", "elevator-amount", "column-amount"],
+	corporate: ["number-of-floors", "number-of-basements","number-of-parking-spots", "number-of-corporations", "maximum-occupancy", "elevator-amount", "column-amount"],
+	hybrid: ["number-of-floors", "number-of-basements", "number-of-companies", "number-of-parking-spots", "maximum-occupancy", "business-hours", "elevator-amount", "column-amount"]
+};
 
-function pick() {
+
+function start() {
+    $("#building-type").change(ChooseType)
+}
+
+
+function ChooseType() {
     if ($("#building-type").val() === "residential") {
-       buildingType = "residential"
-       document.getElementById ("elevatorForm").reset();
-       document.getElementById ("outputForm").reset();
-       
-        $("#number-of-apartments").css("display", "block");
-        $("#number-of-basements").css("display", "block");
-        $("#number-of-floors").css("display", "block");
-        $("#number-of-parking-spots").css("display", "none");
-        $("#number-of-companies").css("display", "none");
-        $("#number-of-elevators").css("display", "none");
-        $("#maximum-occupancy").css("display", "none");
-        $("#business-hours").css("display", "none");
-        $("#number-of-corporations").css("display", "none");
+       buildType = "res"
         
 
     }else if ($("#building-type").val() === "commercial") {
-        buildingType = "commercial"
-        document.getElementById ("elevatorForm").reset ();
-        document.getElementById ("outputForm").reset();
-        $("#number-of-apartments").css("display", "none");
-        $("#number-of-floors").css("display", "block");
-        $("#number-of-basements").css("display", "block");
-        $("#number-of-companies").css("display", "block");
-        $("#number-of-parking-spots").css("display", "block");
-        $("#number-of-elevators").css("display", "block");
-        $("#maximum-occupancy").css("display", "none");
-        $("#business-hours").css("display", "none");
-        $("#number-of-corporations").css("display", "none");
+        console.log("com")
+        buildType = "com"
       
 
     }else if ($("#building-type").val() === "corporate") {
-        buildingType = "corporate"
-        document.getElementById ("elevatorForm").reset ();
-        document.getElementById ("outputForm").reset();
-        $("#number-of-apartments").css("display", "none");
-        $("#number-of-floors").css("display", "block");
-        $("#number-of-basements").css("display", "block");
-        $("#number-of-companies").css("display", "none");
-        $("#number-of-parking-spots").css("display", "block");
-        $("#number-of-elevators").css("display", "none");
-        $("#maximum-occupancy").css("display", "block");
-        $("#number-of-corporations").css("display", "block");
-        $("#business-hours").css("display", "none");
+        console.log("corp")
+        buildType = "corp"
         
 
     }else if ($("#building-type").val() === "hybrid") {
-        buildingType = "hybrid"
-        document.getElementById ("elevatorForm").reset ();
-        document.getElementById ("outputForm").reset();
-        $("#number-of-apartments").css("display", "none");
-        $("#number-of-floors").css("display", "block");
-        $("#number-of-basements").css("display", "block");
-        $("#number-of-companies").css("display", "block");
-        $("#number-of-parking-spots").css("display", "block");
-        $("#number-of-elevators").css("display", "none");
-        $("#maximum-occupancy").css("display", "block");
-        $("#business-hours").css("display", "block");
+        console.log("hyb")
+        buildType = "hyb"
         
     }
 
-    $("#number-of-apartments").on("input", update)
-    $("#number-of-floors").on("input", update)
-    $("#number-of-basements").on("input", update)
-    $("#number-of-elevators").on("input", update)
-    $("#maximum-occupancy").on("input", update)
+    ShowInputs(buildType)
+
 }
 
-let outputValues = {
-    "elevatorsRequired": 1,
-    "columnsRequired": 1,
-    "unitPrice": 1,
-    "installationCosts:": 1,
-    "finalPrice": 1
-}
-
-let cages;
-
-let liftCage = {
-    "standard": 7565,
-    "premium": 12345,
-    "excelium": 15400
-}
-
-
-let installFees = {
-    "standard": .10,
-    "premium": .13,
-    "excelium": .16
-}
-
-function update() {
-    
-    elevatorsNeeded()
-    elevatorUnitPrice()
-    displayResults()
-}
-
-function elevatorsNeeded() {
-   
-    
-    
-    if (buildingType === "residential") {
-        
-        
-        let numOfApts = $("#numofapt").val()
-
-        let numOfFloors = $("#numoffloors").val()
-        
-        if (numOfApts <= 0 || numOfFloors <= 0) {
-            return; 
-        }
-       
-        let avgDoorsPerFloor = Math.ceil(numOfApts / numOfFloors) 
-        
-        let elevatorShaftsRequired = Math.ceil(avgDoorsPerFloor / 6)
-        
-        let columnsRequired = Math.ceil(numOfFloors / 20) 
-
-        outputValues.elevatorsRequired = elevatorShaftsRequired * columnsRequired
-        console.log(elevatorsRequired)
-
-    } else if (buildingType === "commercial") {
-        
-            let elevatorNum = $("#numofelev").val()
-        
-        if (elevatorNum <= 0) {
-            return; 
-        }
-
-        
-        
-        outputValues.elevatorsRequired = elevatorNum
-    
-
-    } else if (buildingType === "corporate" || buildingType === "hybrid") {
-       
-        let occupantsPerFloor = $("#maxocc").val()
-        
-        let numOfFloors = $("#numoffloors").val()
-        
-        let numOfBasements = $("#numofbase").val()
-
-        
-        if (occupantsPerFloor <= 0 || numOfFloors <= 0 || numOfBasements <= 0) {
-            return; 
-        }
-
-        
-        let totalNumOfOccupants = occupantsPerFloor * (Number(numOfFloors) + Number(numOfBasements))
-        
-        let elevatorShaftsRequired = Math.ceil(totalNumOfOccupants / 1000)
-        
-        let columnsRequired = Math.ceil((Number(numOfFloors) + Number(numOfBasements)) / 20)
-       
-        let elevatorsPerColumn = Math.ceil(elevatorShaftsRequired / columnsRequired)
-       
-        elevatorsRequired = elevatorsPerColumn * columnsRequired
-       
+function ShowInputs(type) {
+    switch(type) {
+        case "res":
+            showElements(buildingTypes.residential)
+            break;
+        case "com":
+            showElements(buildingTypes.commercial)
+            break;
+        case "corp":
+            showElements(buildingTypes.corporate)
+            break;
+        case "hyb":
+            showElements(buildingTypes.hybrid)
+            break;
     }
 }
 
-function elevatorUnitPrice() {
-  
-    $("input[name=liftCage]").each(function () {
-       if ($(this).is(':checked')) {
-           cage = $(this).val()
-        
-       }
-    })
+function showElements(elems)
+{
+    hideElements(buildingTypes.residential)
+    hideElements(buildingTypes.commercial)
+    hideElements(buildingTypes.corporate)
+    hideElements(buildingTypes.hybrid)
+
+	elems.forEach(element => {
+        $("#"+element).css("display", "block");
+
+    });
 }
 
-function displayResults() {
-    
-     
-    // Display the recommended number of elevators
-    $("#elevatorNum").val(outputValues.elevatorsRequired)
-
-    // Display the unit price of an elevator
-    $("#unitPrice").val(`$${liftCage[cage].toLocaleString(undefined, { minimumFractionDigits: 2 })}`)
-
-    // Display the total price of the elevators
-    $("#totalPriceElevators").val(`$${(outputValues.elevatorsRequired * liftCage[cage]).toLocaleString(undefined, { minimumFractionDigits: 2 })}`)
-
-    // Display the installation fees
-    let totalPrice = outputValues.elevatorsRequired * liftCage[cage]
-    $("#installFees").val(`$${(totalPrice * installFees[cage]).toLocaleString(undefined, { minimumFractionDigits: 2 })}`)
-
-    // Display the final total price including fees
-    let installFee = totalPrice * installFees[cage]
-    $("#totalPrice").val(`$${(totalPrice + installFee).toLocaleString(undefined, { minimumFractionDigits: 2 })}`)
+function hideElements(elems)
+{
+	elems.forEach(element => {
+        $("#"+element).css("display", "none");
+    });
 }
